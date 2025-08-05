468a9c1906ab734622981a1e7378af6b28e37204
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-05 16:22:18 -0500

    innerHTML

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ async function renderMovies() {
     const data = await response.json()
     const moviesArr = data.Search
     moviesWrapper.innerHTML = moviesArr.map((movie) => {
+        console.log(moviesArr)
         return `
         <div class="movie">
-        <img src= alt="" />
-        <h2>Title</h2>
-        <h4>Year</h4>
+        <img src=${movie.Poster} alt="" />
+        <h2>${movie.Title}</h2>
+        <h4>${movie.Year}</h4>
         <button>Learn More</button>
         </div>
         `
