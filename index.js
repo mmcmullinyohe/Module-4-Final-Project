6e4959a9ec04af058d46018c4512d1038b2ab79e
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-06 10:42:43 -0500

    landing page updated

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const moviesWrapper = document.querySelector('.movies')
 console.log(moviesWrapper)
 
 function searchChange(event) {
-    console.log(event.target.value);
+    console.log(searchChange(event))
 }
 
 async function renderMovies() {
     const response = await fetch (`https://www.omdbapi.com/?apikey=1f242e1c&s=batman`)
     const data = await response.json()
     const moviesArr = data.Search
-    moviesWrapper.innerHTML = moviesArr.map((movie) => {
+    moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
         console.log(moviesArr)
         return `
         <div class="movie">
