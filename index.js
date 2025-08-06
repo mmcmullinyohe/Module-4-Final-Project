bd697ca41935ef3ff87abe2a73ea2efa568a8db3
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-06 15:37:30 -0500

    JS changes

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const moviesWrapper = document.querySelector('.movies')
-
-console.log(moviesWrapper)
+const moviesWrapper = document.querySelector(".movies")
+    console.log(moviesWrapper)
 
 function searchChange(event) {
-    console.log(searchChange(event))
+    console.log(event.target.value)
 }
 
-async function renderMovies() {
-    const response = await fetch (`https://www.omdbapi.com/?apikey=1f242e1c&s=batman`)
+async function renderMovies(){
+    const response = await fetch(`https://www.omdbapi.com/?s=batman&apikey=1f242e1c`)
     const data = await response.json()
-    const moviesArr = data.Search
-    moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
-        console.log(moviesArr)
-        return `
-        <div class="movie">
-        <img src=${movie.Poster} alt="" />
-        <h2>${movie.Title}</h2>
-        <h4>${movie.Year}</h4>
-        <button>Learn More</button>
-        </div>
-        `
-    })
+    const moviesArr = data.searchChange
 }
 
 renderMovies()
