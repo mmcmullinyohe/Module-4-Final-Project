b8dde0a0f31cd2b5fd8c7311f510996c9dd89ea7
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-08 11:20:08 -0500

    movies sort

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+//GRABBING ELEMENTS
 const moviesWrapper = document.querySelector(".movies");
 const searchName = document.querySelector(".searchName");
 
+//GLOBAL MOVIES VARIABLE
+let currentMovies = []
+
+//HANDLING THE SEARCH
 function searchChange(event) {
   renderMovies(event.target.value);
   searchName.innerHTML;
 }
 
+//RENDERING MOVIES / CALLING API
 async function renderMovies(searchTerm) {
   const response = await fetch(
     `https://www.omdbapi.com/?s=${searchTerm}&apikey=1f242e1c`
   );
   const data = await response.json();
-  const moviesArr = data.Search;
-  console.log(moviesArr);
-  displayMovies(moviesArr);
+  currentMovies = data.Search
+  displayMovies(currentMovies);
 }
 
+//DISPLAYING MOVIES
 function displayMovies(movieList) {
   moviesWrapper.innerHTML = movieList
     .slice(0, 6)
@@ -30,4 +36,20 @@ function displayMovies(movieList) {
         `;
     })
     .join("");
+}
+
+//SORTING MOVIES
+function sortChange(event) {
+    const sortOption = event.target.value
+
+    let sortedMovies = [...currentMovies]
+
+    if (sortOption === "newest") {
+        sortedMovies.sort((a, b) => b.Year - a.Year)
+    }
+    else if (sortOption === "oldest") {
+        sortedMovies.sort((a, b) => a.Year - b.Year)
+    }
+
+    displayMovies(sortedMovies);
 }
